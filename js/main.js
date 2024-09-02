6ff529b72491202227d733f1bfb9023580469c5b
No pro bro <65702111+Noprobro11@users.noreply.github.com>
2024-09-02 16:56:33 +0530

    Navbar 2.0

--- js/main.js
+++ js/main.js
@@ -8,3 +8,14 @@ document.getElementById("cards").onmousemove = e => {
       card.style.setProperty("--mouse-y", `${y}px`);
     };
   }
+
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
